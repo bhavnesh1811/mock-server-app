4ba26d4376727cbaca4e8ecc01cb8c3d1c05bd5c
bhavnesh1811 <erbhavnesharora@gmail.com>
2022-10-21 15:34:12 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,19 @@
+//1. npm init -y to initialise npm
+//2. npm i json-server to install json-server
+//3. Create server.js
+
+//4.Copy the template to server.js
+//5. node server.js
+
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () =>
+  console.log(`JSON Server is running on port ${PORT}`)
+);
